03a0143e0efe1b69113f2ae634b9646e8f00ed38
Vo Xuan Vinh <vxvinh89@gmail.com>
2025-07-29 23:29:25 +0700

    Update

--- dist/checkout.asset.php
+++ dist/checkout.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '5c08388ac015e6f7cdee3764dd7d9ba7');
+<?php return array('dependencies' => array('jquery'), 'version' => '3b4a3121ac1a25ac');
--- dist/edit-order.asset.php
+++ dist/edit-order.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'wp-api-fetch', 'wp-date', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '38c17cebb41e0f539075fe0ee8f91d73');
+<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'wp-api-fetch', 'wp-date', 'wp-i18n', 'wp-url'), 'version' => 'e48fffa4657173e2');
