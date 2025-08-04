063a47fb98698ecd79d85a0a841b8487a8c8ced4
Vo Xuan Vinh <vxvinh89@gmail.com>
2025-08-04 11:01:09 +0700

    Update CreateVTPOrder.vue

--- resources/js/vue/components/create/CreateVTPOrder.vue
+++ resources/js/vue/components/create/CreateVTPOrder.vue
@@ -334,7 +334,6 @@ export default {
           this.availableServices = [];
           this.ORDER_SERVICE = null;
         }
-        console.log(newVal.province);
       },
       deep: true
     },
