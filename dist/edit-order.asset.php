ae5e7faf23e9a22760aa9d4eb20a97e855b9fd6e
Vo Xuan Vinh <vxvinh89@gmail.com>
2025-08-04 15:41:54 +0700

    Dist

--- dist/edit-order.asset.php
+++ dist/edit-order.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'wp-api-fetch', 'wp-date', 'wp-i18n', 'wp-url'), 'version' => 'e48fffa4657173e2');
+<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'wp-api-fetch', 'wp-date', 'wp-i18n', 'wp-url'), 'version' => '2a3adf64c89d44c2');
